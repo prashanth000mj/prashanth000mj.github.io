d9e0e8f7fda4411f4014d66ca5320f9af12a8d02
Prashanth MJ <prashanth000mj@gmail.com>
2017-01-21 20:32:31 +0530

    React Setup

---
+++ scripts/SinglePage.js
@@ -0,0 +1,10 @@
+
+class SinglePage extends React.Component {
+    render (){
+        return (
+            <h2> Hello World </h2>
+        );
+    }
+}
+
+ReactDOM.render(<SinglePage/>, document.getElementById('root'));
